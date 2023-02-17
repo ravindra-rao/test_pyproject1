162a3dc042d67c54eb8755d2dffde6ff9ff2ddb7
ravindra.rao <ravindra.r1992@gmail.com>
2023-02-17 02:29:40 -0400

    added comments in app.py

--- test_pyproject1/app.py
+++ test_pyproject1/app.py
@@ -8,7 +8,12 @@ import sys
 
 # from test_pyproject1.core import engine
 # from test_pyproject1.utils import db
-
+'''
+We can use either the above or the below
+In eclipse, mypy gives some error for the below
+But the below runs fine in both eclipse and gitbash
+But the above runs only in eclipse and not in gitbash
+'''
 from core import engine
 from utils import db
 
